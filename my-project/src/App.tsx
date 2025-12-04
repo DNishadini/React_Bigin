d8604f84fb8824663ee4b5f87859045085797680
DNishadini <dinushikaherath150@gmail.com>
2025-12-05 00:18:08 +0530

    Fourth Part

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,16 +1,15 @@
 import "./App.css";
+import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/todos";
 
 function App() {
   return (
     <main className="py-10 bg-red-50 h-screen">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
-      <div>
+      <div className="max-w-lg mx-auto">
         <div className="space-y-2">
           {dummyData.map((todo) => (
-            <p key={todo.id} className="text-lg">
-              {todo.title}
-            </p>
+            <TodoItem todo={todo} />
           ))}
         </div>
       </div>
