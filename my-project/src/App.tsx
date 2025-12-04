92b439d1f0a523a9017f2a05f7cf20a6907f2091
DNishadini <dinushikaherath150@gmail.com>
2025-12-04 23:13:08 +0530

    First Part

---
+++ my-project/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="py-10 bg-red-50 h-screen">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+    </main>
+  );
+}
+
+export default App;
