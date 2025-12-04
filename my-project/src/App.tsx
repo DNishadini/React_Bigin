7713dde24dff4e5c56f1d4fe9b710d94bddf7dac
DNishadini <dinushikaherath150@gmail.com>
2025-12-04 23:55:49 +0530

    Third Part

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -6,9 +6,11 @@ function App() {
     <main className="py-10 bg-red-50 h-screen">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
       <div>
-        <div>
+        <div className="space-y-2">
           {dummyData.map((todo) => (
-            <p key={todo.id}>{todo.title}</p>
+            <p key={todo.id} className="text-lg">
+              {todo.title}
+            </p>
           ))}
         </div>
       </div>
