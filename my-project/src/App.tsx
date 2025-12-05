4c3b06fc12c1abbe25265af24c1f196abeaa44f7
DNishadini <dinushikaherath150@gmail.com>
2025-12-06 00:22:35 +0530

    Final part

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -4,6 +4,7 @@ import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/todos";
 import AddTodoForm from "./components/AddTodoForm";
 import TodoList from "./components/TodoList";
+import TodoSummary from "./components/TodoSummary";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -15,7 +16,7 @@ function App() {
   function addTodo(title: string) {
     setTodos((prevTodos) => [
       {
-        id: prevTodos.length + 1,
+        id: Date.now(),
         title,
         completed: false,
       },
@@ -24,6 +25,9 @@ function App() {
   }
   function deleteTodo(id: number) {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }
+  function deleteAllCompletedTodos() {
+    setTodos((prevTodos) => prevTodos.filter((todo) => !todo.completed));
   }
   return (
     <main className="py-10 bg-red-50 h-screen space-y-5">
@@ -36,6 +40,7 @@ function App() {
           onDelete={deleteTodo}
         />
       </div>
+      <TodoSummary todos={todos} deleteAllCompleted={deleteAllCompletedTodos} />
     </main>
   );
 }
--- my-project/src/components/TodoList.tsx
+++ my-project/src/components/TodoList.tsx
@@ -18,15 +18,22 @@ export default function TodoList({
     return a.completed ? 1 : -1;
   });
   return (
-    <div className="space-y-2">
-      {todosSorted.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          onCompletedChange={onCompletedChange}
-          onDelete={onDelete}
-        />
-      ))}
-    </div>
+    <>
+      <div className="space-y-2">
+        {todosSorted.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onCompletedChange={onCompletedChange}
+            onDelete={onDelete}
+          />
+        ))}
+      </div>
+      {todos.length === 0 && (
+        <p className="text-center text-sm text-gray-500">
+          No todos yet. Add a new one above
+        </p>
+      )}
+    </>
   );
 }
